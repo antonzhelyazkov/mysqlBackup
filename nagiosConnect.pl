5839acb31049ff735d04d38f4f2c65582236929a
root <antonisimo@gmail.com>
2016-09-30 16:19:43 +0300

    nagios connect beta

--- nagiosConnect.pl
+++ nagiosConnect.pl
@@ -14,9 +14,14 @@ my $infFile;
 my $processDefault = "mysqlBackup.pl";
 my @infLines;
 my $lastLinesElementNumber;
+my $startTime;
+my $cronInterval;
+my $cronTimeBuffer = 7200;
+my $errorCounter = 0;
 
-GetOptions (    "inf-file=s"    => \$infFile,
-                "process=s"     => \$process) or exit(2);
+GetOptions (    "inf-file=s"   		=> \$infFile,
+		"cron-interval=i"	=> \$cronInterval,
+                "process=s"		=> \$process) or exit(2);
 
 if (!defined($infFile)) {
 	$infFile = $infFileDefault;
@@ -41,33 +46,40 @@ if (open(INF, '<:encoding(UTF-8)', $infFile)) {
 
 $infLines[0] =~ s/PID\s//;
 $pidNumber = $infLines[0];
+$startTime = $infLines[1];
+$startTime =~ s/startTime\s//g;
 $lastLinesElementNumber = scalar(@infLines);
 
 foreach my $proc (@{$ref->table}) {
 	if ( $pidNumber == $proc->{pid} && $proc->{cmndline} =~ m/$process/){
-		print $proc->{pid};
-		print $proc->{cmndline} ."\n";
+#		print $proc->{pid};
+#		print $proc->{cmndline} ."\n";
 		$found = 1;
 		print "OK file $infFile exists and process $proc->{cmndline} with ID $proc->{pid} is running | status=1";
 		exit(0);
 	}
 }
 
+if (defined($cronInterval)){
+	my $cronIntervalSeconds = $cronInterval * 3600;
+	if (time() > ($cronIntervalSeconds + $startTime + $cronTimeBuffer)) {
+		print "WARNING backup has not started more than ".($cronInterval + $cronTimeBuffer / 3600)." hours Possible backup problem!\n";
+		exit(1);
+	}
+}
+
 if ($infLines[$lastLinesElementNumber - 1] !~ m/endTime\s\d+/ && $found == 0 ) {
-	print "WARNING no end timestamp in $infFile and running process $process. Possible backup problem!| status=2\n";
+	print "WARNING no end timestamp in $infFile and running process $process. Possible backup problem!\n";
 	exit(1);
 }
 
 for ( my $i = 2; $i < scalar(@infLines) - 2; $i++ ) {
-	print $infLines[$i];
+	if ( $infLines[$i] =~ m/^Error/) {
+		$errorCounter++;
+	}
 }
 
-#} else {
-#        print "OK file $pidFile does not exists | status=0";
-#        exit(0);
-#}
-#
-#if ( !$found ) {
-#        print "WARNING file $pidFile exists but no process $process with ID $pidNumber is running | status=2";
-#        exit(1);
-#}
+if ( $errorCounter > 0 ) {
+	print "WARNING finished with $errorCounter errors. Possible backup problem!\n";
+        exit(1);
+}
