422395b21a66082f198bb20cc70f2aad162ebb9d
root <root@backup-mysql.photo-forum.net>
2018-11-19 15:09:55 +0200

    r1

--- mysqlBackup.sh
+++ mysqlBackup.sh
@@ -11,7 +11,7 @@
 # changelog
 # ver 0.2.10 - 16.11.2018
 
-OPTS=$(getopt -o vhtl --long verbose,help,tables,local-copy,local-copy-path:,local-copy-days:,mysql-root-password: -n 'parse-options' -- "$@")
+OPTS=$(getopt -o vhtl --long verbose,help,tables,local-copy,local-copy-path:,local-copy-days:,mysql-root-password:,ftp-host:,ftp-user:,ftp-pass: -n 'parse-options' -- "$@")
 getOptsExitCode=$?
 if [ $getOptsExitCode != 0 ]; then
 	echo "Failed parsing options." >&2 ;
@@ -20,9 +20,10 @@ fi
 
 eval set -- "$OPTS"
 
-localCopy=0
+localCopy=1
 localCopyPath="/var/tmp"
 localCopyDays=1
+localBackupDays=$(date +%Y%m%d%H%M -d "$localCopyDays day ago")
 verbose=1
 mysqlHost="localhost"
 HELP=false
@@ -34,6 +35,9 @@ while true; do
 		--mysql-root-password ) mysqlRootPassword="$2"; shift; shift ;;
 		--local-copy-path ) localCopyPath="$2"; shift; shift ;;
 		--local-copy-days ) localCopyDays="$2"; shift; shift ;;
+		--ftp-host ) ftpHost="$2"; shift; shift ;;
+		--ftp-user ) ftpUser="$2"; shift; shift ;;
+		--ftp-pass ) ftpPass="$2"; shift; shift ;;
 		-t | --tables ) tables=1; shift ;;
 		-v | --verbose ) verbose=1; shift ;;
 		-h | --help ) HELP=true; shift ;;
@@ -54,17 +58,16 @@ scriptLog="/var/log/$ownScriptName.log"
 nagiosLog="/var/log/$ownScriptName.nagios"
 lastRun="/var/log/$ownScriptName.last"
 
-ftpHost="212.73.140.112";
-ftpUser="photo-media"
-ftpPass='7kZt66qwYn3GT4qz'
-
 ftpRemotePath="/$serverName-mysql-backup/"
 rateLimit="12048K"
+# Speed is in bytes per second. 0 - means unlimited
+ftpUploadSpeed=0
+ftpDownloadSpeed=0
+keepRemoteBackupDays=5
+remoteBackupDays=$(date +%Y%m%d%H%M -d "$keepRemoteBackupDays day ago")
 
 mysqlDir="$localCopyPath/mysql"
 currentBackupDir="$mysqlDir/$(date +%Y%m%d%H%M)"
-
-isSalve=1
 
 ########################################################
 
@@ -73,9 +76,13 @@ displayHelp() {
 	echo
 	echo "	-v,	--verbose		Run script in verbose mode"
 	echo "	-l,	--local-copy		Leave local copy"
+	echo "	-t,	--tables		Split archive by tables"
 	echo "		--local-copy-path	Directory where local copy is stored"
 	echo "		--local-copy-days	Backup keep days"
 	echo "		--mysql-root-password	MySQL/MariaDB root password"
+	echo "		--ftp-host		ftp host"
+	echo "		--ftp-user		ftp user"
+	echo "		--ftp-pass		ftp password"
 	echo
 # echo some stuff here for the -a or --add-options 
 	exit 1
@@ -130,7 +137,7 @@ mkdir $currentDBDir
 checkMDDBDir=$?
 if [ $checkMDDBDir -ne 0 ]
 then
-	logPrint "could not create directory $currentDBDir" 1 1
+	logPrint "ERROR could not create directory $currentDBDir" 1 1
 fi
 
 logPrint "current directory $currentDBDir" 0 0
@@ -151,12 +158,49 @@ done
 
 }
 
+function remoteTransfer() {
+
+hash lftp 2>/dev/null
+lftpCheck=$?
+if [ $lftpCheck -ne 0 ]
+then
+        rm -f $nagiosLog
+        logPrint "ERROR lftp not found!" 1 1
+fi
+
+logPrint "start ftp transfer" 0 0
+lftp -u $ftpUser:$ftpPass $ftpHost -e "set net:connection-limit 1; set net:limit-rate $ftpDownloadSpeed:$ftpUploadSpeed; mirror -R $currentBackupDir $hostname/; bye"
+checkUploadExit=$?
+if [ $checkUploadExit -ne 0 ]; then
+	logPrint "ERROR in upload" 1 1
+else
+	logPrint "ftp transfer finished successfully" 0 0
+fi
+
+for currentRemoteDirectory in $(curl -s -u $ftpUser:$ftpPass ftp://$ftpHost/$hostname/ -X MLSD | grep 'type=dir' | cut -d';' -f8)
+do
+	if [[ $currentRemoteDirectory =~ ^[0-9]{12}$ ]]; then
+		if [ $currentRemoteDirectory -lt $remoteBackupDays ]; then
+			if [ ! -z $currentRemoteDirectory ]; then
+				logPrint "remove $hostname/$currentRemoteDirectory" 0 0
+				lftp -u $ftpUser:$ftpPass $ftpHost -e "rm -r $hostname/$currentRemoteDirectory; bye"
+				checkRemoteRemove=$?
+				if [ $checkRemoteRemove -ne 0 ]; then
+					logPrint "ERROR could not remove remote directory $hostname/$currentRemoteDirectory" 1 0
+				fi
+			fi
+		fi
+	fi
+done
+
+}
+
 ########################################################
 
 logPrint START 0 0
 
 if [ -f $nagiosLog ]; then
-        logPrint "file $nagiosLog exists EXIT!" 1 1
+        logPrint "ERROR file $nagiosLog exists EXIT!" 1 1
 else
         echo $$ > $nagiosLog
 fi
@@ -192,7 +236,7 @@ if [ ! -d $localCopyPath ]; then
 	checkMKdirLocal=$?
 	if [ $checkMKdirLocal -ne 0 ]
 	then
-		logPrint "could not create directory $localCopyPath" 1 1
+		logPrint "ERROR could not create directory $localCopyPath" 1 1
 	fi
 fi
 
@@ -202,7 +246,7 @@ if [ ! -d $currentBackupDir ]; then
         checkMKdirCurrent=$?
         if [ $checkMKdirCurrent -ne 0 ]
         then
-                logPrint "could not create directory $currentBackupDir" 1 1
+                logPrint "ERROR could not create directory $currentBackupDir" 1 1
         fi
 fi
 
@@ -210,9 +254,8 @@ hash pigz 2>/dev/null
 pigzCheck=$?
 if [ $pigzCheck -ne 0 ]
 then
-	logPrint "pigz not found!" 0 0
 	rm -f $nagiosLog
-	exit 1
+	logPrint "ERROR pigz not found!" 1 1
 fi
 
 logPrint "checking local mysql connection" 0 0
@@ -290,7 +333,7 @@ else
 			fi
 		done
 	else
-		logPrint "Error in slave stopping" 1 1
+		logPrint "ERROR in slave stopping" 1 1
 	fi
 	$mysqlConnString "start slave"
 	sleep 1
@@ -299,6 +342,39 @@ else
 fi
 
 echo ${#ERRORS[@]}
+
+if [ ! -z $ftpHost ] && [ ! -z $ftpUser ] && [ ! -z $ftpPass ]
+then
+	logPrint "Start FTP Upload $ftpHost" 0 0
+	remoteTransfer
+else
+	logPrint "FTP transfer not configured or missing parameter" 0 0
+fi
+
+if [ $localCopy -eq 1 ]; then
+localBackupDirs=$(ls $mysqlDir)
+for directory in $localBackupDirs ; do
+	if [[ $directory =~ ^[0-9]{12}$ ]]; then
+		if [ $directory -lt $localBackupDays ]; then
+			removeDir=$mysqlDir/$directory
+			logPrint "check if directory exists $removeDir" 0 0
+			if [ -d $removeDir ] && [ ! -z $directory ] ; then
+				logPrint "remove $removeDir" 0 0
+				rm -rf $removeDir
+				removeDirStatus=$?
+				logPrint "remove status $removeDirStatus" 0 0
+				if [ $removeDirStatus -ne 0 ]; then
+					logPrint "ERROR could not remove directory $removeDir" 1 1
+				fi
+			else
+				logPrint "ERROR directory $removeDir does not exist. Someting went wrong" 1 1
+			fi
+		fi
+	fi
+done
+else
+        rm -rf $mysqlDir/*
+fi
 
 ########################################################
 
